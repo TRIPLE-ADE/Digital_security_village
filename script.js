0055b3b39ca43fa8e0da8e635052105ada5fb680
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2022-08-23 04:37:11 +0100

    update

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-const 
+const hamburger =document.querySelector('.hamburger');
+const navMenu = document.getElementById('nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-link').forEach(e => e.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
+}));
