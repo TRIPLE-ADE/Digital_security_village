46f4835171f4c2c931b182ac6acd700aa63d27db
TRIPLE-ADE <adetomiwaabdul@gmail.com>
2022-08-25 19:12:37 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const hamburger =document.querySelector('.hamburger');
 const navMenu = document.getElementById('nav-links');
 
+const revealLeft = document.querySelectorAll('.reveal-left');
+
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
@@ -10,3 +12,21 @@ document.querySelectorAll('.nav-link').forEach(e => e.addEventListener('click', 
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }));
+
+function reveal() {
+  let reveals = document.querySelectorAll(".reveal");
+  for (let i = 0; i < reveals.length; i++) {
+    let windowHeight = window.innerHeight;
+    let elementTop = reveals[i].getBoundingClientRect().top;
+    let elementVisible = 100;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+
